abd032a55d046e6a1fd17e208bd6d70f451abe68
Kahlil Lechelt <hello@kahlil.info>
2022-02-01 17:16:58 +0100

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: ["layouts/**/*.html"],
+  content: ["layouts/**/*.html"],
   theme: {
     flex: {
-      "1": "1 1 0%",
+      1: "1 1 0%",
       auto: "1 1 auto",
       initial: "0 1 auto",
       none: "none",
